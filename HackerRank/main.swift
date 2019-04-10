6f0a59afc42c30937903711cce865549157c5430
SeokWoo Lee <skwu1@naver.com>
2019-04-10 14:49:31 +0900

    HackerRank Project 생성

---
+++ HackerRank/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  HackerRank
+//
+//  Created by SeokWoo Lee on 10/04/2019.
+//  Copyright © 2019 SeokWoo Lee. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
