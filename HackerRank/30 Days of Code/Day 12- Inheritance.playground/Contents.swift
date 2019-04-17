3fc3cb19b5d53a4ebf85690560f320faa4e2691e
SeokWoo Lee <skwu1@naver.com>
2019-04-17 10:58:26 +0900

    Day 12: Inheritance

---
+++ HackerRank/30 Days of Code/Day 12- Inheritance.playground/Contents.swift
@@ -0,0 +1,69 @@
+import Foundation
+
+// Class Person
+class Person {
+    private let firstName: String
+    private let lastName: String
+    private let id: Int
+    
+    // Initializer
+    init(firstName: String, lastName: String, id: Int) {
+        self.firstName = firstName
+        self.lastName = lastName
+        self.id = id
+    }
+    
+    // Print person data
+    func printPerson() {
+        print("Name: \(lastName), \(firstName)")
+        print("ID: \(id)")
+    }
+} // End of class Person
+
+// Class Student
+class Student: Person {
+    var testScores: [Int]
+    
+    /*
+     *   Initializer
+     *
+     *   Parameters:
+     *   firstName - A string denoting the Person's first name.
+     *   lastName - A string denoting the Person's last name.
+     *   id - An integer denoting the Person's ID number.
+     *   scores - An array of integers denoting the Person's test scores.
+     */
+    // Write your initializer here
+    init(firstName: String, lastName: String, id: Int, scores: [Int]) {
+        self.testScores = scores
+        
+        super.init(firstName: firstName, lastName: lastName, id: id)
+    }
+    
+    /*
+     *   Method Name: calculate
+     *   Return: A character denoting the grade.
+     */
+    // Write your method here
+    func calculate() -> Character {
+        let total = testScores.reduce(0, +)
+        let average = Double(total) / Double(testScores.count)
+        
+        switch average {
+        case 90...100:
+            return "O"
+        case 80..<90:
+            return "E"
+        case 70..<80:
+            return "A"
+        case 55..<70:
+            return "P"
+        case 40..<55:
+            return "D"
+        default:
+            return "T"
+        }
+    }
+} // End of class Student
+
+let nameAndID = readLine()!.components(separatedBy: " ")
