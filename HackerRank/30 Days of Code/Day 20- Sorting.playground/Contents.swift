d2a7ec8090b7aa8a549b60bdd206618bef6681b3
SeokWoo Lee <skwu1@naver.com>
2019-04-25 00:48:14 +0900

    Day 20: Sorting

---
+++ HackerRank/30 Days of Code/Day 20- Sorting.playground/Contents.swift
@@ -0,0 +1,28 @@
+import Foundation
+
+// read the integer n
+let n = Int(readLine()!)!
+
+// read the array
+var arr = readLine()!.components(separatedBy: " ").map{ Int($0)! }
+
+var numberOfSwaps = 0
+
+for _ in 0..<n {
+    
+    for j in 0..<(n-1) {
+        if arr[j] > arr[j + 1] {
+            arr.swapAt(j, j + 1)
+            
+            numberOfSwaps += 1
+        }
+    }
+    
+    if numberOfSwaps == 0 {
+        break
+    }
+}
+
+print("Array is sorted in \(numberOfSwaps) swaps.")
+print("First Element: \(arr[0])")
+print("Last Element: \(arr[n-1])")
